fcdaedad52181b0fba5fbf4d48a055011c17889d
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2025-07-09 15:11:54 +0100

    git commit -m "first commit"

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Pricing Page',
+    description: 'Esta es la página de precios de nuestro servicio',
+    keywords: ["About Page", "RubenGonz", "Información", "..."],
+};
+
+export default function PricingPage() {
+    return (
+        <span className="text-7xl">Pricing Page</span>
+    );
+}
